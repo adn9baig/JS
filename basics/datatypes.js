63693efeba12c19dc8948d9c632c9a5d8fd06dc4
adn9baig <99684396+adn9baig@users.noreply.github.com>
2024-07-20 05:59:19 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"
+
+
+let name = "hitesh"
+let age = 18
+let isLogged = false
+
+//number => 2 to power 53
+//bigint
+//String => ""
+//boolean => true/false
+//null => standalone value
+//undefine => 
+// symbol    (uniquess) most used in react
+
+
+//object
+
+console.log(typeof "Adnan");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+

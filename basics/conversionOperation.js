3025b96a857da0a4d058903e48b2fc3756853bcb
adn9baig <99684396+adn9baig@users.noreply.github.com>
2024-07-20 06:33:19 +0000

    typeconversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueIntNumber = Number(score)
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);
+
+
+// "33" => 33
+// "33abc" => NaN  (Not a number)
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);

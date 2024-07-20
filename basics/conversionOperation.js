cbd006ff316846c0f4758343d94417ba7914c1dc
adn9baig <99684396+adn9baig@users.noreply.github.com>
2024-07-20 11:48:33 +0000

    typeconversions and operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -16,7 +16,56 @@ let score = "33"
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33
-let StringNumber = String(someNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let someNumber = 33
+// let StringNumber = String(someNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+//   **************************OPERATION *****************************************************//
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "Adnan"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "23");
+
+
+//Increment Decrement
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+
+let x = 5
+y = x++
+
+// console.log(x);
+// console.log(y);
+
+
+
+
+
+let a = 5
+b = ++a
+// console.log(a);
+// console.log(b);
+
+
+
+
+
+

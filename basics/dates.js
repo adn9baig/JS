68fe2c259a4507cfce7e89f99cb4cd4e09cac9b4
adn9baig <99684396+adn9baig@users.noreply.github.com>
2024-07-28 15:42:51 +0000

    dates

---
+++ basics/dates.js
@@ -0,0 +1,14 @@
+let myDates = new Date();
+// console.log(myDates);
+// console.log(myDates.toISOString());
+
+// let myCreatedDate = new Date(2001,8,9);
+// console.log(myCreatedDate.toDateString());
+
+
+let myCreatedDate = new Date(2001,8,9);
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
